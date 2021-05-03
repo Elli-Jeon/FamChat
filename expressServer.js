c6d36973d067b199ed94c76f040463eb248496e4
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-05-03 20:46:02 +0900

    require socket

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 
 app.use(express.static("public"));
 
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/chat.html");
 })
+
+io.on("connection",(socket)=>{
+    console.log("socket is connected");
+
+    socket.on("new message",(data)=>{
+        socket.broadcast.emit("")
+    })
+
+})
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`Listening at port : ${port}`);
